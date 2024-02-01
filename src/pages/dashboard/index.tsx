71df4982676d493c740fe1339fbd33ca06038b10
RaphaelSalayog <rsalayog0199@gmail.com>
2024-02-01 22:44:09 +0800

    feat: add some text in dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -10,6 +10,14 @@ const Dashboard = () => {
         Pure front-end, no back-end included yet but it is functional.
       </p>
       <p style={{ marginBottom: "1rem" }}>
+        It is just a recreation of our capstone project with my own code and
+        design to showcase my knowledge about React.js
+      </p>
+      <p style={{ marginBottom: "1rem" }}>
+        Tech Stack : HTML, CSS, JavaScript, React.js, Redux, Next.js,
+        TypeScript, Ant Design
+      </p>
+      <p style={{ marginBottom: "1rem" }}>
         Feel free to try the available features
       </p>
     </MainLayout>
